35583bb380437e3aff9f8f412fd32b2f992e8933
Mazen Naji <mazen.naji.1@gmail.com>
2026-05-09 12:34:08 +0300

    feat: add custom login page with enhanced styling and error handling

--- dashboard/config/settings.py
+++ dashboard/config/settings.py
@@ -8,6 +8,7 @@ DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -69,8 +70,77 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR.parent / "docs" / "assets",
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CEDAR_API_URL = os.getenv("CEDAR_API_URL", "http://localhost:8000")
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Cedar Admin",
+    "site_header": "Cedar Chatbot",
+    "site_brand": "Cedar",
+    "site_logo": "cedar-logo.png",
+    "site_logo_classes": "img-fluid",
+    "site_icon": "cedar-logo.png",
+    "login_logo": None,
+    "login_logo_dark": None,
+    "welcome_sign": "",
+    "copyright": "Cedar Chatbot",
+    "search_model": ["auth.User"],
+    "custom_css": "admin/css/login_fix.css",
+    "topmenu_links": [
+        {"name": "Dashboard", "url": "admin:index"},
+        {"name": "View Site", "url": "/"},
+        {"name": "API", "url": "http://localhost:8000/docs", "new_window": True},
+    ],
+    "icons": {
+        "auth.User": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "core.ChatMessage": "fas fa-comments",
+        "core.ChatSession": "fas fa-layer-group",
+        "core.UserFeedback": "fas fa-star",
+        "core.BotConfiguration": "fas fa-cog",
+    },
+    "default_icon_parents": "fas fa-folder",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": True,
+    "show_ui_builder": False,
+    "changeform_format": "horizontal_tabs",
+    "language_chooser": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-success",
+    "accent": "accent-olive",
+    "navbar": "navbar-dark",
+    "no_navbar_border": True,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-olive",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": True,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": "darkly",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
+    },
+}
